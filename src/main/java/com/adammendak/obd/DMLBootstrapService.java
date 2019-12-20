42c610e66d4448718b8f92865d060e69044f05b4
adammendak <adam.mendak@gmail.com>
2019-12-20 12:16:34 +0100

    version v2

--- src/main/java/com/adammendak/obd/DMLBootstrapService.java
+++ src/main/java/com/adammendak/obd/DMLBootstrapService.java
@@ -34,7 +34,7 @@ class DMLBootstrapService {
             statement.executeUpdate(String.format(TRUNCATE_TEMPLATE, "OCENA"));
             for (int i = 1; i < 12; i++) {
                 String sql = String.format(INSERT_TEMPLATE, "OCENA",
-                        i + ", '" + GRADE_DESCRIPTIVE[i - 1] + "', '" + GRADE_FLOAT[i - 1] + "'");
+                        i + ", '" + GRADE_DESCRIPTIVE[i - 1] + "', " + GRADE_FLOAT[i - 1] + "");
                 statement.executeUpdate(sql);
             }
 
--- src/main/java/com/adammendak/obd/ForeignKeySimulationException.java
+++ src/main/java/com/adammendak/obd/ForeignKeySimulationException.java
@@ -2,6 +2,8 @@ package com.adammendak.obd;
 
 public class ForeignKeySimulationException extends Exception {
 
+    static final long serialVersionUID = -3387516993124229948L;
+
     public ForeignKeySimulationException(String message) {
         super(message);
     }
